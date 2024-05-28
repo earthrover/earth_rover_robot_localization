66cd522ef498923e393f084b610c93ed3de761f2
Josep Bello <josep.bello@earthrover.farm>
2024-05-28 16:16:12 +0200

    Changed default world frame id to map

--- src/navsat_transform.cpp
+++ src/navsat_transform.cpp
@@ -63,7 +63,7 @@ namespace RobotLocalization
     base_link_frame_id_("base_link"),
     gps_frame_id_(""),
     utm_zone_(0),
-    world_frame_id_("odom"),
+    world_frame_id_("map"),
     transform_timeout_(ros::Duration(0)),
     tf_listener_(tf_buffer_)
   {
